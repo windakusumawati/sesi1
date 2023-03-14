3507f3dcacb21ddd1a57f35dd151f348dac7d12d
windakusumawati <127836384+windakusumawati@users.noreply.github.com>
2023-03-14 17:02:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML sesi 1</title>
+</head>
+<body>
+ 
+    <h3> Belajar Web Progamming<?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>Menggunakan Script javascript</h3>");
+    </script>
+
+
+<?php
+    echo "<h3> Menggunakan Scrip PHP </h3>";
+?>
+</body>
+</html>
